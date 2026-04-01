906ce810bef83dde729c37e5905285a5f82f102e
jpbatleidenuniv <j.p.baars@umail.leidenuniv.nl>
2026-04-01 18:08:26 +0200

    Added improved baseline to experiments, not ran yet

--- Assignments/TvsD/exploration.py
+++ Assignments/TvsD/exploration.py
@@ -38,36 +38,43 @@ class ExplorationSpec:
 
 EXPLORATIONS = [
     # Baseline
-    ExplorationSpec("Baseline"),
-    # Exploration policy
-    ExplorationSpec("Epsilon_0.01", epsilon=0.01),
-    ExplorationSpec("Epsilon_0.05", epsilon=0.05),
-    ExplorationSpec("Epsilon_0.2", epsilon=0.2),
     ExplorationSpec(
-        "Softmax_T0.5", policy="softmax", temperature=0.5
-    ),
-    ExplorationSpec(
-        "Softmax_T1.0", policy="softmax", temperature=1.0
-    ),
-    ExplorationSpec(
-        "Softmax_T2.0", policy="softmax", temperature=2.0
-    ),
-    # Learning rate
-    ExplorationSpec("LR_1e-3", lr=1e-3),
-    ExplorationSpec(
-        "LR_5e-4", lr=5e-4
-    ),  # same as baseline, explicit
-    ExplorationSpec("LR_1e-4", lr=1e-4),
-    ExplorationSpec("LR_1e-5", lr=1e-5),
-    # Batch size
-    ExplorationSpec("Batch_1", batch_size=1),
-    ExplorationSpec("Batch_10", batch_size=10),
-    ExplorationSpec("Batch_20", batch_size=20),
-    ExplorationSpec("Batch_64", batch_size=64),
-    # Network architecture
-    ExplorationSpec("Layers_1_W64", layers=1, width=64),
-    ExplorationSpec("Layers_2_W64", layers=2, width=64),
-    ExplorationSpec("Layers_3_W128", layers=3, width=64),
+        "BaselineImproved_NN3-128_Batch24_SoftmaxT2.0",
+        policy="softmax",
+        temperature=2.0,
+        batch_size=24,
+        layers=3,
+        width=128,
+    )
+    # # Exploration policy
+    # ExplorationSpec("Epsilon_0.01", epsilon=0.01),
+    # ExplorationSpec("Epsilon_0.05", epsilon=0.05),
+    # ExplorationSpec("Epsilon_0.2", epsilon=0.2),
+    # ExplorationSpec(
+    #     "Softmax_T0.5", policy="softmax", temperature=0.5
+    # ),
+    # ExplorationSpec(
+    #     "Softmax_T1.0", policy="softmax", temperature=1.0
+    # ),
+    # ExplorationSpec(
+    #     "Softmax_T2.0", policy="softmax", temperature=2.0
+    # ),
+    # # Learning rate
+    # ExplorationSpec("LR_1e-3", lr=1e-3),
+    # ExplorationSpec(
+    #     "LR_5e-4", lr=5e-4
+    # ),  # same as baseline, explicit
+    # ExplorationSpec("LR_1e-4", lr=1e-4),
+    # ExplorationSpec("LR_1e-5", lr=1e-5),
+    # # Batch size
+    # ExplorationSpec("Batch_1", batch_size=1),
+    # ExplorationSpec("Batch_10", batch_size=10),
+    # ExplorationSpec("Batch_20", batch_size=20),
+    # ExplorationSpec("Batch_64", batch_size=64),
+    # # Network architecture
+    # ExplorationSpec("Layers_1_W64", layers=1, width=64),
+    # ExplorationSpec("Layers_2_W64", layers=2, width=64),
+    # ExplorationSpec("Layers_3_W128", layers=3, width=64),
     # # Target network
     # ExplorationSpec("Target_Update10",  target_network=True, update_target=10),
     # ExplorationSpec("Target_Update50",  target_network=True, update_target=50),
